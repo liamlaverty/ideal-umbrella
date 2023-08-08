f11dd13a1aa4029ad8002a7141e5dc245b8affce
Liam Laverty <liamlaverty@gmail.com>
2023-08-08 15:05:13 +0100

    starts to add a viewcomponent for maps

---
+++ IdealUmbrella.site/ViewComponents/MapboxViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdealUmbrella.site.ViewComponents
+{
+    public class MapboxViewComponent : ViewComponent
+    {
+        public MapboxViewComponent()
+        {
+            
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(new MapboxViewComponentDto
+            {
+                FrontendKey = ""
+            });
+        }
+    }
+
+    public class MapboxViewComponentDto
+    {
+        public string FrontendKey { get; set; }
+    }
+}
