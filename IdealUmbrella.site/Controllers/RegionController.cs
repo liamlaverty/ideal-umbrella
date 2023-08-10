b2f27b438ade41bd375932ace6dfee6f9f8d886e
Liam Laverty <liamlaverty@gmail.com>
2023-08-10 12:30:39 +0100

    updates the importer class to insert country content

--- IdealUmbrella.site/Controllers/RegionController.cs
+++ IdealUmbrella.site/Controllers/RegionController.cs
@@ -1,11 +1,19 @@
 ﻿using IdealUmbrella.DataConnector.CountryData;
 using IdealUmbrella.DataConnector.Models.CsvModels;
+using IdealUmbrella.site.Helpers.PropertyTypeHelpers;
 using IdealUmbrella.site.Models.Config;
+using IdealUmbrella.site.Models.Exceptions;
+using J2N.Text;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using System.Drawing;
+using System.Text;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Persistence.Querying;
 using Umbraco.Cms.Web.Common.Controllers;
 using Umbraco.Cms.Web.Common.PublishedModels;
 
@@ -43,12 +51,94 @@ namespace IdealUmbrella.site.Controllers
             var regionCollectionDoc = rootChildren.First(c => c.ContentType.Alias == RegionCollection.ModelTypeAlias);
 
             var allCountries = _countryDataCsv.GetCountries();
+            foreach (var country in allCountries)
+            {
+                AddOrUpdateCountry(regionCollectionDoc, country);
+            }
     
             return "OK" + allCountries.Count();
         }
+
+
+        /// <summary>
+        /// Upserts a country document 
+        /// 
+        /// 
+        /// Checks if a country already exists, if so, overwrites its data with the CSV's data
+        /// otherwise, creates a new country document, and inserts the CSV data into that
+        /// </summary>
+        /// <param name="parentDoc">The parent <see cref="RegionCollection"/> document this Country should be inserted into</param>
+        /// <param name="country">The country to be upserted</param>
+        /// <exception cref="ArgumentNullException">A parameter was null</exception>
+        /// <exception cref="InvalidDocumentTypeException">The parent document type was of an invalid type</exception>
+        /// <exception cref="NullReferenceException"></exception>
+        private void AddOrUpdateCountry(IContent parentDoc, CsvCountryDto country)
+        {
+            if (parentDoc is null)
+            {
+                throw new ArgumentNullException(nameof(parentDoc));
+            }
+            if (parentDoc.ContentType.Alias != RegionCollection.ModelTypeAlias)
+            {
+                throw new InvalidDocumentTypeException($"Parent document must be of type {RegionCollection.ModelTypeAlias}");
+            }
+            if (country is null)
+            {
+                throw new ArgumentNullException(nameof(country));
+            }
+
+            int pageSize = 100;
+            _contentService.GetPagedChildren(parentDoc.Id, 0, 1, out long countOfChildren);
+            var countOfPages = Math.Ceiling((decimal)countOfChildren / pageSize);
+
+            IContent regionToUpsertAsContent = null;
+
+            bool pageExists = false;
+            while (pageExists == false)
+            {
+                // page through the results, 
+                for (int pageIndex = 0; pageIndex < countOfPages; pageIndex++)
+                {
+                    var pages = _contentService.GetPagedChildren(parentDoc.Id, pageIndex, pageSize, out _);
+                    if (pages.Any(c => c.Name == country.Name))
+                    {
+                        pageExists = true;
+                        regionToUpsertAsContent = pages.First(c => c.Name == country.Name);
+                        break;
+                    }
+                }
+                break;
+            }
+            if (!pageExists)
+            {
+                // If we've made it to the end of the loop, there's no content item with that name inthe paged results
+                // so we need to create one
+                regionToUpsertAsContent = _contentService.Create(country.Name, parentDoc.Id, RegionSingle.ModelTypeAlias);
+            }
+
+            if (regionToUpsertAsContent == null)
+            {
+                // verify that the region item was either found in the while loop, 
+                // or is newly created
+                throw new NullReferenceException(nameof(regionToUpsertAsContent));
+            }
+
+            regionToUpsertAsContent.SetValue("countryName", country.Name);
+            regionToUpsertAsContent.SetValue("pageTitle", $"{country.Name}'s CO2e Emissions");
+            regionToUpsertAsContent.SetValue("SEODescription", $"Detailed CO2e Emissions for '{country.Name}'.");
+            regionToUpsertAsContent.SetValue("countryAlpha2", country.Alpha2);
+            regionToUpsertAsContent.SetValue("countryAlpha3", country.Alpha3);
+            regionToUpsertAsContent.SetValue("countryCode", country.CountryCode);
+            regionToUpsertAsContent.SetValue("Regions", RepeatableTextStringHelper.FormatRepeatableTextStringProperty(new List<string>
+            {
+                country.Region,
+                country.IntermediateRegion,
+                country.SubRegion
+            }));
+
+            _contentService.SaveAndPublish(regionToUpsertAsContent);
+        }
     }
-
-
 
     public class RegionDto
     {
---
+++ IdealUmbrella.site/Helpers/PropertyTypeHelpers/RepeatableTextStringHelper.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace IdealUmbrella.site.Helpers.PropertyTypeHelpers
+{
+    internal static class RepeatableTextStringHelper
+    {
+        /// <summary>
+        /// Takes a list of strings, and formats them for the RepeatableTextString content property
+        /// </summary>
+        /// <param name="items">A <see cref="List{string}"/> of items to insert into the RepeatableTextString 
+        /// content property
+        /// </param>
+        /// <seealso cref="RepeatableTextStringHelper.FormatRepeatableTextStringContentFromList(IEnumerable{string} items)"/>
+        /// <seealso cref="https://docs.umbraco.com/umbraco-cms/fundamentals/backoffice/property-editors/built-in-umbraco-property-editors/multiple-textbox"/>
+        /// <returns>A well formatted string, including NewLines</returns>
+        public static string FormatRepeatableTextStringProperty(List<string> items)
+        {
+            return RepeatableTextStringContent(items);
+        }
+
+        /// <summary>
+        /// Takes an IEnumerable of strings, and formats them for the RepeatableTextString content property
+        /// </summary>
+        /// <param name="items">A <see cref="IEnumerable{string}"/> of items to insert into the RepeatableTextString 
+        /// content property
+        /// </param>
+        /// <seealso cref="https://docs.umbraco.com/umbraco-cms/fundamentals/backoffice/property-editors/built-in-umbraco-property-editors/multiple-textbox"/>
+        /// <seealso cref="RepeatableTextStringHelper.FormatRepeatableTextStringContentFromList(List{string} items)"/>
+        /// <returns>A well formatted string, including NewLines</returns>
+        public static string FormatRepeatableTextStringProperty(IEnumerable<string> items)
+        {
+            return RepeatableTextStringContent(items);
+        }
+
+
+        /// <summary>
+        /// provides a single method for formatting textstrings
+        /// </summary>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        private static string RepeatableTextStringContent(IEnumerable<string> items)
+        {
+            if (items is null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            var sb = new StringBuilder();
+
+            sb.Append(items.ElementAt(0));
+
+            for (int i = 1; i < items.Count(); i++)
+            {
+                if (!string.IsNullOrEmpty(items.ElementAt(i)))
+                {
+                    sb.Append($"{Environment.NewLine}{items.ElementAt(i).Trim()}");
+                }
+            }
+            return sb.ToString();
+        }
+
+        
+
+    }
+}
