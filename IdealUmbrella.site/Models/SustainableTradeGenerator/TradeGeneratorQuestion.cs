61b383ff74310d58ffe9e3a797aa20d2714c0838
LFLaverty <liamlaverty+imobile@gmail.com>
2023-12-14 12:10:45 +0000

    adds a simple form generator, rebuilds the models after upgrading to v13

---
+++ IdealUmbrella.site/Models/SustainableTradeGenerator/TradeGeneratorQuestion.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IdealUmbrella.site.Models.SustainableTradeGenerator
+{
+    public class TradeGeneratorQuestion
+    {
+        public string Title{ get; set; }
+        public string Dimension { get; set; }
+
+        public string Label { get
+            {
+                return $"{Dimension} {Title.ToLower()}";
+            } }
+
+        /// <summary>
+        ///  Gets a html friendly string from the title/dimension
+        /// </summary>
+        public string HtmlID
+        {
+            get
+            {
+                var result = $"{Dimension}_{Title}";
+                result = result.ToLower();
+                result = Regex.Replace(result, @"[^A-Za-z0-9\s-]", "");
+                result = Regex.Replace(result, @"\s+", " ").Trim();
+                result = Regex.Replace(result, @"\s", "-");
+                return result;
+            }
+        }
+    }
+}
