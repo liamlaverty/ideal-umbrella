b23206b465721422056e1bd0e4f2c58725a24ff5
LFLaverty <liamlaverty+imobile@gmail.com>
2023-12-15 10:18:47 +0000

    starts to move to viewcomponents for TradeMatrix objects

---
+++ IdealUmbrella.site/ViewComponents/TradeAssessmentMatrixComponents/TradeAssessmentMatrixViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdealUmbrella.site.ViewComponents.TradeAssessmentMatrixComponents
+{
+    public class TradeAssessmentMatrixGradeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string grade)
+        {
+            return View("TradeGradeSpan", grade);
+        }
+    }
+
+
+    public class TradeAssessmentMatrixViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string grade)
+        {
+            return View("TradeGradeMatrix", grade);
+        }
+    }
+}
