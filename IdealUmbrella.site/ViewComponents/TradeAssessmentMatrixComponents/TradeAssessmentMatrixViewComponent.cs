ae8fd993db5281ee035dee92aa7926fc50a7c3d7
LFLaverty <liamlaverty+imobile@gmail.com>
2023-12-15 10:26:07 +0000

    moves from partials to view components

--- IdealUmbrella.site/ViewComponents/TradeAssessmentMatrixComponents/TradeAssessmentMatrixViewComponent.cs
+++ IdealUmbrella.site/ViewComponents/TradeAssessmentMatrixComponents/TradeAssessmentMatrixViewComponent.cs
@@ -2,6 +2,21 @@
 
 namespace IdealUmbrella.site.ViewComponents.TradeAssessmentMatrixComponents
 {
+
+    /// <summary>
+    /// Renders a full TradeMatrix
+    /// </summary>
+    public class TradeAssessmentMatrixViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string grade)
+        {
+            return View("TradeGradeMatrix", grade);
+        }
+    }
+
+    /// <summary>
+    ///  Renders a TradeMatrix branded A/B/N/U
+    /// </summary>
     public class TradeAssessmentMatrixGradeViewComponent : ViewComponent
     {
         public IViewComponentResult Invoke(string grade)
@@ -11,11 +26,5 @@ namespace IdealUmbrella.site.ViewComponents.TradeAssessmentMatrixComponents
     }
 
 
-    public class TradeAssessmentMatrixViewComponent : ViewComponent
-    {
-        public IViewComponentResult Invoke(string grade)
-        {
-            return View("TradeGradeMatrix", grade);
-        }
-    }
+    
 }
